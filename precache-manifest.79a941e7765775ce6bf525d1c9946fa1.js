77953fe7ae61fe5bfd3dbeddae7cec32ac783203
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2019-12-05 09:27:06 +0530

    Demo

---
+++ precache-manifest.79a941e7765775ce6bf525d1c9946fa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de5fa5de30e59ef0eb4914f60645f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a4f46466f62fff772c",
+    "url": "/static/css/main.cd91152e.chunk.css"
+  },
+  {
+    "revision": "e2ce2e637a745f5b38de",
+    "url": "/static/js/2.207b1811.chunk.js"
+  },
+  {
+    "revision": "42a4f46466f62fff772c",
+    "url": "/static/js/main.b0742009.chunk.js"
+  },
+  {
+    "revision": "b3f1906f011740233ed0",
+    "url": "/static/js/runtime-main.153f8a4c.js"
+  }
+]);
